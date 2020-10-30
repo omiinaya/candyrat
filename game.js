b70d742a2069a7ac080d83bbc608ca834d1265cd
Omar Minaya <omiinaya@gmail.com>
2020-10-30 19:39:59 -0400

    adjusted width and height of player.

--- game.js
+++ game.js
@@ -321,7 +321,8 @@ Game.Door.prototype.constructor = Game.Door;
 
 Game.Player = function (x, y) {
 
-  Game.MovingObject.call(this, x, y, 7, 12);
+  //set width and height of the player.
+  Game.MovingObject.call(this, x, y, 15, 15);
 
   Game.Animator.call(this, Game.Player.prototype.frame_sets["idle-right"], 10);
 
@@ -419,12 +420,12 @@ Game.TileSet = function (columns, tile_size, tile_scale) {
 
   let f = Game.Frame;
 
-  this.frames = [new f(115, 96, 13, 16, 0, -4), // idle-left
-  new f(50, 96, 13, 16, 0, -4), // jump-left
-  new f(102, 96, 13, 16, 0, -4), new f(89, 96, 13, 16, 0, -4), new f(76, 96, 13, 16, 0, -4), new f(63, 96, 13, 16, 0, -4), // walk-left
-  new f(0, 112, 13, 16, 0, -4), // idle-right
-  new f(65, 112, 13, 16, 0, -4), // jump-right
-  new f(13, 112, 13, 16, 0, -4), new f(26, 112, 13, 16, 0, -4), new f(39, 112, 13, 16, 0, -4), new f(52, 112, 13, 16, 0, -4), // walk-right
+  this.frames = [new f(115, 96, 13, 16, 0, 0), // idle-left
+  new f(50, 96, 13, 16, 0, 0), // jump-left
+  new f(102, 96, 13, 16, 0, 0), new f(89, 96, 13, 16, 0, 0), new f(76, 96, 13, 16, 0, 0), new f(63, 96, 13, 16, 0, 0), // walk-left
+  new f(0, 112, 13, 16, 0, 0), // idle-right
+  new f(65, 112, 13, 16, 0, 0), // jump-right
+  new f(13, 112, 13, 16, 0, 0), new f(26, 112, 13, 16, 0, 0), new f(39, 112, 13, 16, 0, 0), new f(52, 112, 13, 16, 0, 0), // walk-right
   new f(81, 112, 14, 16), new f(96, 112, 16, 16), // carrot
   
   ];
